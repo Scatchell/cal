c37a6d5ad7545d5f20996a89474012e1ee6f8258
Anthony Scatchell <Anthony.Scatchell@spartasystems.com>
2015-12-30 18:35:45 -0500

    Added services page and pushpin/scrollspy plugins for scrolling through services

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var sassMiddleware = require('node-sass-middleware');
 
-var routes = require('./routes/index');
+var routes = require('./routes/pages');
 var users = require('./routes/users');
 
 var app = express();
--- public/javascripts/global.js
+++ public/javascripts/global.js
@@ -3,8 +3,19 @@
  * Created by anthonyscatchell on 12/28/15.
  */
 
-$(document).ready(function(){
+$(document).ready(function () {
     $('.parallax').parallax();
+
     $('.slider').slider({full_width: true});
+
+    $('.right-nav-wrapper').pushpin(
+        {
+            top: $('.right-nav').offset().top,
+            bottom: $('footer').offset().top - $('.right-nav-wrapper').height(),
+            offset: 15
+        }
+    );
+
+    $('.scroll-spy').scrollSpy();
 });
 
