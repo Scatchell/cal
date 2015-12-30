9da9831187a5314e24f18058e294f94849cecc60
Anthony Scatchell <Anthony.Scatchell@spartasystems.com>
2015-12-30 15:06:10 -0500

    Removed some colorings and used sass variables instead, also adding slider and more to accordians

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Care-A-Lot' });
+  res.render('index', { });
 });
 
 module.exports = router;
