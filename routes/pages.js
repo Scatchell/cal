1db2663b7c5b49b64bef4c8059667cadd29b5256
Anthony Scatchell <Anthony.Scatchell@spartasystems.com>
2015-12-31 21:51:15 -0500

    Renaming services to classrooms

--- routes/pages.js
+++ routes/pages.js
@@ -5,8 +5,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { });
 });
 
-router.get('/services', function(req, res, next) {
-  res.render('services', { });
+router.get('/classrooms', function(req, res, next) {
+  res.render('classrooms', { });
 });
 
 module.exports = router;
