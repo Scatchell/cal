2123feacef425540ae19935bb035837c6e95f181
Anthony Scatchell <Anthony.Scatchell@spartasystems.com>
2016-01-08 19:05:59 -0500

    Not initializing scrollspy unless enabled
    Meant to avoid bug created by trying to initialize scrollspy when no
    scrollspy elements exist on page (when loading about-us page with
    default tab not having any scroll spy elements)

--- public/javascripts/about-us.js
+++ public/javascripts/about-us.js
@@ -1,13 +1,17 @@
+var initializeIfScrollSpyEnabled = function (selectedTabLink) {
+    var selectedTab = selectedTabLink.parent();
+    if (selectedTab.hasClass('scroll-spy-enabled')) {
+        initializeScrollSpy(selectedTabLink.attr('href'));
+    }
+};
+
 $(document).ready(function () {
     //pre initialize scroll spy in case user loaded page directly on a tab that needs it
     var $currentlyActiveTab = $('li.tab .active');
-    initializeScrollSpy($currentlyActiveTab.attr('href'));
+    initializeIfScrollSpyEnabled($currentlyActiveTab);
 
     //attach scroll spy initialization to each other tab that needs to reinitialize
     $('ul.tabs').on('tab_change', function (event, selectedTabLink) {
-        var selectedTab = $(selectedTabLink).parent();
-        if (selectedTab.hasClass('scroll-spy-enabled')) {
-            initializeScrollSpy(selectedTab.find('a').attr('href'));
-        }
+        initializeIfScrollSpyEnabled($(selectedTabLink));
     });
 });
