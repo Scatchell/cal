22fee51be65468e100d2d275e2167fbf55768e1d
Anthony Scatchell <scatchell@gmail.com>
2016-12-29 22:14:13 -0500

    Fixing reveal directory change and adding necessary fonts for reveal

--- public/javascripts/virtual-tour.js
+++ public/javascripts/virtual-tour.js
@@ -3,7 +3,7 @@ var link = document.createElement('link');
 link.rel = 'stylesheet';
 link.type = 'text/css';
 link.href = window.location.search.match(/print-pdf/gi) ? 'css/print/pdf.css' : 'css/print/paper.css';
-document.getElementsByTagName('head')[0].appendChild(link);
+// document.getElementsByTagName('head')[0].appendChild(link);
 
 // Full list of configuration options available at:
 // https://github.com/hakimel/reveal.js#configuration
@@ -16,26 +16,26 @@ Reveal.initialize({
     // Optional reveal.js plugins
     dependencies: [
         {
-            src: '/javascripts/revealjs/classList.js', condition: function () {
+            src: '/javascripts/lib/revealjs/classList.js', condition: function () {
             return !document.body.classList;
         }
         },
         {
-            src: '/javascripts/revealjs/plugin/markdown/marked.js', condition: function () {
+            src: '/javascripts/lib/revealjs/plugin/markdown/marked.js', condition: function () {
             return !!document.querySelector('[data-markdown]');
         }
         },
         {
-            src: '/javascripts/revealjs/plugin/markdown/markdown.js', condition: function () {
+            src: '/javascripts/lib/revealjs/plugin/markdown/markdown.js', condition: function () {
             return !!document.querySelector('[data-markdown]');
         }
         },
         {
-            src: '/javascripts/revealjs/plugin/highlight/highlight.js', async: true, callback: function () {
+            src: '/javascripts/lib/revealjs/plugin/highlight/highlight.js', async: true, callback: function () {
             hljs.initHighlightingOnLoad();
         }
         },
-        {src: '/javascripts/revealjs/plugin/zoom-js/zoom.js', async: true},
-        {src: '/javascripts/revealjs/plugin/notes/notes.js', async: true}
+        {src: '/javascripts/lib/revealjs/plugin/zoom-js/zoom.js', async: true},
+        {src: '/javascripts/lib/revealjs/plugin/notes/notes.js', async: true}
     ]
 });
