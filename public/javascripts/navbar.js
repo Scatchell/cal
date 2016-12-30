ac84bb6076ec23e57bbdcb186826c86a1cefea83
Anthony Scatchell <scatchell@gmail.com>
2016-12-29 21:50:18 -0500

    Fixing tab switching on about us dropdown links and moving some external js into lib folder

---
+++ public/javascripts/navbar.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    $("#about-us-locations").click(function () {
+        $('ul.tabs').tabs('select_tab', 'locations');
+    });
+    $("#about-us-philosophy").click(function () {
+        $('ul.tabs').tabs('select_tab', 'philosophy');
+    });
+    $("#about-us-model").click(function () {
+        $('ul.tabs').tabs('select_tab', 'model');
+    });
+    $("#about-us-curriculum").click(function () {
+        $('ul.tabs').tabs('select_tab', 'curriculum');
+    });
+
+});
