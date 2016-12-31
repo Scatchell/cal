d0c906c02cb741bd77aa0425e88acc763f0d2c38
Anthony Scatchell <scatchell@gmail.com>
2016-12-31 14:16:18 -0500

    Fixing confusing about-us top link

--- public/javascripts/navbar-footer.js
+++ public/javascripts/navbar-footer.js
@@ -1,15 +1,14 @@
 $(document).ready(function () {
-    $("#about-us-locations").click(function () {
+    $(".about-us-locations").click(function () {
         $('ul.tabs').tabs('select_tab', 'locations');
     });
-    $("#about-us-philosophy").click(function () {
+    $(".about-us-philosophy").click(function () {
         $('ul.tabs').tabs('select_tab', 'philosophy');
     });
-    $("#about-us-model").click(function () {
+    $(".about-us-model").click(function () {
         $('ul.tabs').tabs('select_tab', 'model');
     });
-    $("#about-us-curriculum").click(function () {
+    $(".about-us-curriculum").click(function () {
         $('ul.tabs').tabs('select_tab', 'curriculum');
     });
-
 });
