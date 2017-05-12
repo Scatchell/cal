051489ff8f34dd88085c2798996109e29bbc7e48
Anthony Scatchell <scatchell@gmail.com>
2017-05-13 00:22:36 +0530

    Making sure scroll reminder doesn't show on any mobile device

--- public/javascripts/scroll-arrow.js
+++ public/javascripts/scroll-arrow.js
@@ -1,8 +1,27 @@
 $(document).ready(function () {
+    var cardContent = $('.container');
+    if (isElementPartiallyVisible(cardContent)) {
+        $('#scroll-down').hide();
+    }
+
     $(window).scroll(function (event) {
-        var scroll = $(window).scrollTop();
-        if(scroll > 200) {
+        if (isElementPartiallyVisible(cardContent)) {
             $('#scroll-down').hide();
         }
     });
 });
+
+function isElementPartiallyVisible(el) {
+
+    //handler for jquery
+    if (typeof jQuery === "function" && el instanceof jQuery) {
+        el = el[0];
+    }
+
+    var rect = el.getBoundingClientRect();
+
+    return (
+        rect.top >= 0 &&
+        rect.top <= (window.innerHeight || document.documentElement.clientHeight) - 60
+    );
+}
